430b6a8b7dc6968aeb99048708740d88ad384b08
afsals <afsals@qburst.com>
2018-10-26 08:39:25 +0530

    Change bubble sort

--- datastructures/arrays/bubble_sort.py
+++ datastructures/arrays/bubble_sort.py
@@ -8,19 +8,22 @@ application: For academic purpose only
 
 
 def bubble_sort_algorithm(a):
-    for i in range(len(a)-1): 
+    number_of_iteration = 0
+    for i in range(len(a)-1):
         swapped = False
-        for j in range((len(a)-i-1)):
-            if (a[j] > a[j+1]):
+        for j in range(len(a)-1-i):
+            number_of_iteration += 1
+            if a[j]>a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
                 swapped = True
         if not swapped:
             break
+    print("number_of_iteration: ", number_of_iteration)
     return a
 
 
 if __name__ == '__main__':
-    a = list(range(100))
+    a = list(range(10))
     shuffle(a)
     print(a)
     b = bubble_sort_algorithm(a)
